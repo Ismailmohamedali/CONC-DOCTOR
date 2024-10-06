9a679b31d9e2c68756c660640520d23b29327f5e
Your Name <you@example.com>
2024-10-06 15:32:22 +0300

    first commit

---
+++ CONTACT-DOCTORS/Controllers/DoctorController.cs
@@ -0,0 +1,15 @@
+﻿using CONTACT_DOCTORS.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CONTACT_DOCTORS.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult Index()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var doc =  context.Doctors.ToList();
+            return View(model: doc);
+        }
+    }
+}
